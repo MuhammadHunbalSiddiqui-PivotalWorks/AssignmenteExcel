b04d41c5a1ae429d9e2e61acfad06364e9ae517c
MuhammadHunbalSiddiqui-PivotalWorks <hunbal.siddiqui@pivotalworks.io>
2022-01-30 16:49:28 +0500

    validation added.

--- excelAssignment.js
+++ excelAssignment.js
@@ -1,6 +1,8 @@
 const convertor = (columnHeading) => {
     let ans = 0;
-
+    if (!isNaN(columnHeading)) {
+        return "Wrong column provided"
+    }
     columnHeading = columnHeading.toUpperCase()
     for (let i = 0; i < columnHeading.length; i++) {
         ans *= 26; // because A-Z ---> 1-26
@@ -13,5 +15,6 @@ const convertor = (columnHeading) => {
 // Output BA = 53
 // Output A = 1
 // Output AAA = 703
+// Output ABC = 731
 
-console.log("Answer", convertor("AAA"))
+console.log("Answer", convertor("ABC"))
